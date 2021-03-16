ec7ca7c06fb817dd0ee2fa474aab2b6833757bc5
cdfishe1 <75449671+cdfishe1@users.noreply.github.com>
2021-03-15 19:34:37 -0500

    Add global variables

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,15 +1,8 @@
 const omdbKey = '1c14b455';
-
-const getOmdb = () => {
-    const getOmdbApi = `https://www.omdbapi.com/?i=tt3896198&apikey=${omdbKey}`;
-
-    fetch(getOmdbApi)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    console.log(data);
-  });
-};
-
-getOmdb();
+const tmdbKey = '94406989928ec2b2df604a84ef1604a1';
+const title = document.querySelector('#title');
+const year = document.querySelector('#year');
+const submitBtn = document.querySelector('#submitBtn');
+const submitBtnTmdb = document.querySelector('#submitBtnTmdb');
+const showType = document.querySelector('#showType');
+const results = document.querySelector('#results');
