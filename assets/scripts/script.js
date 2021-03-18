bf288595610c01a2afaba60f2052d9f5e58c237d
cdfishe1 <75449671+cdfishe1@users.noreply.github.com>
2021-03-17 19:27:23 -0500

    Update variables

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,9 +1,12 @@
+//variables
+
 const omdbKey = '1c14b455';
 const tmdbKey = '94406989928ec2b2df604a84ef1604a1';
 const title = document.querySelector('#title');
 const year = document.querySelector('#year');
 const submitBtn = document.querySelector('#submitBtn');
 const submitBtnTmdb = document.querySelector('#submitBtnTmdb');
+const refineType = document.querySelector('#refineSearch');
 const showType = document.querySelector('#showType');
 const results = document.querySelector('#results');
 
